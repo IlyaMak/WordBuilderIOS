5b9ccfcce733310e47085aae51a0561b9c677c24
Ilya <imakarskij@gmail.com>
2022-11-23 08:15:20 -0800

    Make nice moving from Register screen to Level screen

--- WordBuilder/ui/Registration.swift
+++ WordBuilder/ui/Registration.swift
@@ -10,38 +10,45 @@ import RealmSwift
 
 struct Registration: View {
     @State private var name = ""
-//    @EnvironmentObject var viewRouter: ViewRouter
-    @State private var showView = false
+    @State private var showLevelView = false
+    @State private var showLeaderboardView = false
     
     var body: some View {
-        VStack {
-            Text("Hello \(name)")
-            TextField("Enter your name", text: $name).textFieldStyle(RoundedBorderTextFieldStyle()).padding()
-                    
-            Button(
-                action: {
-                    let application = Application()
-                    application.name = name
-                    application.token = "1234567890"
-                    
-                    let realm = try! Realm()
-                    try! realm.write {
-                        realm.add(application)
+        NavigationView {
+            VStack {
+                Text("Hello \(name)")
+                TextField("Enter your name", text: $name).textFieldStyle(RoundedBorderTextFieldStyle()).padding()
+                        
+                Button(
+                    action: {
+                        let application = Application()
+                        application.name = name
+                        application.token = "1234567890"
+                        
+                        let realm = try! Realm()
+                        try! realm.write {
+                            realm.add(application)
+                        }
+                        self.showLevelView.toggle()
+                }, label: {
+                    Text("Register".uppercased())
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(
+                            Color.purple
+                                .cornerRadius(10)
+                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        )
+                    })
+                
+                NavigationLink(
+                    destination: LevelScreen().navigationBarHidden(true),
+                    isActive: $showLevelView,
+                    label: {
+                        
                     }
-                    self.showView.toggle()
-            }, label: {
-                Text("Register".uppercased())
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(
-                        Color.purple
-                            .cornerRadius(10)
-                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                    )
-                })
-            .sheet(isPresented: $showView) {
-                LevelScreen()
+                )
             }
         }
     }
